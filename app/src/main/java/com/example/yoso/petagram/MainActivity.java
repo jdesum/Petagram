bf6ad80731b086ccedbff5896d342401d4064504
Jordi Desumvila <jdrcoursera@gmail.com>
2016-05-01 19:52:47 +0200

    Floating Action Button1

--- app/src/main/java/com/example/yoso/petagram/MainActivity.java
+++ app/src/main/java/com/example/yoso/petagram/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yoso.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Llamada del método FAB
+        agregarFAB();
+    }
+
+    // Método agregar FAB
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
+        // En el momento de cliquear el FAB
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
